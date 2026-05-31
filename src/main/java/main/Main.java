82659937c69dc156fe810b7484c6689060ec189a
David <david.willyam23@gmail.com>
2026-05-30 21:01:26 -0300

    Arquitetura do projeto

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,21 @@
+package main;
+
+import java.awt.EventQueue;
+import view.TelaLogin;
+
+public class Main {
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaLogin frame = new TelaLogin();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+}
---
+++ src/main/java/utils/GeradorPDF.java
@@ -0,0 +1,20 @@
+package utils;
+
+public class GeradorPDF {
+	
+	public static void gerarPDF() {
+		// TODO: IMPLEMENTAR O MÉTODO DE GERAÇÃO DE PDFS
+		
+		// OBS: PROVAVELEMNTE O TIPO DO RETORNO VAI SER ALTERADO TALVEZ PARA STRING
+	}
+
+	private class Cursor{
+		int x;
+		int y;
+		
+		public Cursor(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+}
